e47eb978d3a0b7cd36e3603eae2ad6a8b9c66b1a
RaySilvaJS <tobizinhoofc@gmail.com>
2025-05-21 09:01:38 -0300

    pull

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ async function connectToWhatsApp() {
         isConnected = true;
         console.log("Conectado ao WhatsApp");
         conn.sendMessage(destinatario, {
-          text: "Conexão estabelecida com sucesso!",
+          text: "*Conexão estabelecida com sucesso!*",
         });
       }
 
